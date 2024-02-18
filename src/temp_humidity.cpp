f2cf5b07268855c9f2106ea57d177379b483248f
Konrad Münch <konrad@munech.engineer>
2024-02-18 01:34:08 +0100

    added credentials.hpp.template

--- src/temp_humidity.cpp
+++ src/temp_humidity.cpp
@@ -10,13 +10,14 @@ void setupDHT()
 
 float startDHTMonitoring()
 {
+  float offset_temperature = 1;
   float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
+  float temperature = dht.readTemperature() - offset_temperature;
 
   if (isnan(humidity) || isnan(temperature))
   {
     Serial.println(F("Failed to read from DHT sensor!"));
-    return -1.0; 
+    return -1.0;
   }
 
   Serial.print(F("Starting DHT Monitoring... Humidity: "));
@@ -25,11 +26,11 @@ float startDHTMonitoring()
   Serial.print(temperature);
   Serial.println(F("°C "));
 
-  return temperature; 
+  return temperature;
 }
 
 float stopDHTMonitoring()
 {
   Serial.println(F("Stopping DHT Monitoring."));
-  return 0.0; 
+  return 0.0;
 }
